e67b083d0e0e7eb2dcbc6dd8a64d0866814cfcf8
nafri-dev <mdirfantreasurer@gmail.com>
2025-06-30 12:48:00 +0530

    Solved CORS Issues

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,10 @@ const app = express()
 app.use(helmet())
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.VITE_FRONTEND_URL,
     credentials: true,
+     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 )
 
